f41de4a312aa95429367c8170430c712e968c136
Ava Wu <avawuyf@hotmail.com>
2020-08-26 21:36:49 +1000

    My first commit of all code

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+
+Page({
+  data: {
+    imgUrls: [
+'http://cd.visitmelbourne.com/-/media/images/melbourne/things-to-do/history-and-heritage/flinders-street-station_mel_r_1460109_1600x900.jpg?ts=20150925090343&amp;cp=95&w=1200', 'https://static.domain.com.au/domainblog/uploads/2016/04/17233405/2_gl05l2.jpg',
+      'https://res.cloudinary.com/redballoon/c_fill,f_auto/v1523942375/NEW%20Subcat%20Hero%20Images/things-to-do-in-melbourne.jpg'
+    ],
+    //background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    //vertical: false,
+    autoplay: true,
+    circular: true,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
+  },
+})
